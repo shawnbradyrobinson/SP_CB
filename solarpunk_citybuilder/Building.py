acc0141fd375d18f6184ceb8e55898bbbb187d05
Shawn Robinson <shawnbradyrobinson1@gmail.com>
2026-06-12 14:00:37 -0500

    an adjustable continuous timing system

--- solarpunk_citybuilder/Building.py
+++ solarpunk_citybuilder/Building.py
@@ -3,25 +3,20 @@ import Entity as Entity
 
 class Building(Entity):
     "Generic starting point for all building types"
-    def __init__(self, height: int, width: int, materials_needed: dict):
-        Entity.Entity.__init__(self)
+    def __init__(self, building_name: str, height: int, width: int, materials_needed: dict):
+        Entity.Entity.__init__(self, building_name)
         self.height = height
         self.width = width
         self.materials_needed = materials_needed
 
 
-
+    #Placeholder for any given building's "thing" that happens on continuous update 
+    # so then in the game loop we can just have a class that says "for each building, run building.effect()"
     def effect():
         pass 
 
     def canConstruct() -> bool:
         #if Player.inventory includes stuff in the materials_needed dict, then true
-        
-        
         return True 
-
-
-
-
 
     pass
--- solarpunk_citybuilder/Game.py
+++ solarpunk_citybuilder/Game.py
@@ -1,19 +1,21 @@
 import pygame as pygame 
 import Entity as Entity 
 import EntitySprite as EntitySprite 
+import TimeSystem 
 
 class Game: 
     
     def __init__(self):
         pygame.init()
-        self.display_surface = pygame.display.set_mode((1000, 1000))
+        self.display_surface = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("SOLAR PUNK CITY BUILDER")
         self.clock = pygame.time.Clock()
         self.dt = 0 
+        self.TimeSys = TimeSystem.TimeSystem()
 
     def run(self):
         
-        test_surface = pygame.Surface((1000,1000)).convert_alpha()
+        test_surface = pygame.Surface((1200,800)).convert_alpha()
         test_surface.fill("light blue")
         
     
@@ -26,6 +28,14 @@ class Game:
         second_entity = Entity.Entity("second!")
         second_entity_sprite = pygame.sprite.GroupSingle()
         second_entity_sprite.add(EntitySprite.EntitySprite(second_entity, "orange", 300, 300))
+
+
+
+        time_font = pygame.font.Font(None, 24)
+        time_string = "N/A"
+        time_display_surface = time_font.render(time_string, False, "Black")
+        counter = 0 
+        delta_set = 1 
         
         while True:
             # event loop 
@@ -36,20 +46,40 @@ class Game:
                 
                 keys = pygame.key.get_pressed()
 
+                if keys[pygame.K_RIGHT]:
+                    self.TimeSys.changeDelta(self.TimeSys.delta * 2)
+                
+                if keys[pygame.K_LEFT]:
+                    self.TimeSys.changeDelta(self.TimeSys.delta * .5)
+
+                if keys[pygame.K_DOWN]:
+                    if self.TimeSys.delta == 0:
+                        self.TimeSys.changeDelta(1)
+                    else: 
+                        self.TimeSys.changeDelta(0)
+
                 if keys[pygame.K_q]:
                     pygame.quit()
                     exit()
                 
-                self.display_surface.blit(test_surface, (0,0))
-            
-                test_entity_sprite.draw(self.display_surface)
-                test_entity_sprite.update()
-                second_entity_sprite.draw(self.display_surface)
-                second_entity_sprite.update()
+            self.display_surface.blit(test_surface, (0,0))
+
+            test_entity_sprite.draw(self.display_surface)
+            test_entity_sprite.update()
                 
-                pygame.display.update()
+            second_entity_sprite.draw(self.display_surface)
+            second_entity_sprite.update()
+
+            self.TimeSys.nextInstant()
+
+
+            time_string = str(self.TimeSys.counter)
+            time_display_surface = time_font.render(time_string, False, "Black")
+            self.display_surface.blit(time_display_surface, (1000, 15))
                 
-                self.dt = self.clock.tick(60) / 1000 #limits FPS to 60
+            pygame.display.update()
+                
+            self.dt = self.clock.tick(60) / 1000 #limits FPS to 60
 
 
             
