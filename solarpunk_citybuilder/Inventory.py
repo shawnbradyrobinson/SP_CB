4d7953df3040097e2d912ea4602d8e7cc22942e0
Shawn Robinson <shawnbradyrobinson1@gmail.com>
2026-06-16 20:17:56 -0500

    baseline inventory items for each domain, to be expaned upon later

--- solarpunk_citybuilder/Inventory.py
+++ solarpunk_citybuilder/Inventory.py
@@ -5,20 +5,44 @@
 class Inventory:
     def __init__(self):
         items = {
-            "healthcare" :  {
-                vapor_rub = 0
+            "healthcare": {
+                "vapor_rubs": 0,
+                "sticky_aids": 0, 
+                "cough_syrup": 0,
             },
 
             "materials": {
-                oak_planks = 0 
+                "oak_planks": 0, 
+                "bricks": 0, 
+                "aluminum": 0, 
+                "iron": 0, 
+                "copper":  0, 
+                "steel": 0, 
+                "pvc_pipes": 0, 
             },
 
             "food": {
-                eggs = 0
+                "eggs": 0,
+                "beef": 0,
+                "pork": 0,
+                "veal": 0, 
+                "poultry": 0, 
+                "kale": 0, 
+                "tomatoes": 0, 
+                "apples": 0, 
+                "oranges": 0, 
+                "milk_liters": 0, 
+                "spinach": 0, 
+                "tea_leaves": 0, 
+                "coffee_beans": 0, 
+                "water_liters": 0, 
+
             },
 
             "scholarship": {
-                geometry_textbook = 0 
+                "geometry_textbook": 0,
+                "history_textbook": 0,
+
             }
         }
     
@@ -27,9 +51,12 @@ class Inventory:
 
     #DOWN HERE IS WHERE WE CAN SET UP QUICK-ADD PRESETS FOR DIFFERENT SITUATIONS 
 
-    def emptyPreset():
+    def emptyPreset(self):
         pass
 
-    #And so on and so forth... 
-    def allFoodPreset():
+    #And so on and so forth...  
+    def allFoodPreset(self):
         pass
+
+    def trulyRandomPreset(self):
+        pass 
